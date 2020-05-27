e2d242c4068f2d7214c2a9212936ca7caab7c677
marco <mar.calavaro@gmail.com>
2020-05-28 00:18:32 +0200

    Skeleton

--- app/src/main/java/it/thewalkingthread/dbconstructor/MainActivity.java
+++ app/src/main/java/it/thewalkingthread/dbconstructor/MainActivity.java
@@ -1,18 +1,48 @@
 package it.thewalkingthread.dbconstructor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import it.thewalkingthread.dbconstructor.model.Pokemon;
 
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
     //DatabaseReference reference;
-
+    public static final int NUMPOKE = 807;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //use push
+
+        setDb();
 
 
     }
+
+    private void setDb(){
+        Pokemon poke;
+        for (int i = 1 ; i < NUMPOKE;i++){
+            poke = getPokemon(i);
+            setLocalDb(poke);
+            setFirebaseDb(poke);
+        }
+
+    }
+
+    private void setFirebaseDb(Pokemon poke) {
+        //TODO funzione che setta il pokemon in Firebase
+    }
+
+    private void setLocalDb(Pokemon poke) {
+        //TODO funzione che setta il pokemon locale
+    }
+
+    private Pokemon getPokemon(int poke){
+        //TODO funzione che chiama pokeApi
+
+        //chiamata a json
+        return null;//da moificare
+    }
+
 }
