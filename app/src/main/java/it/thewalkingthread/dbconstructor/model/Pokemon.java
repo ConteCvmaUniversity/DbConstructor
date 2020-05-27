724710ff7a9f1be9e1c4a5f01bca67f8c97e0b0f
marco <mar.calavaro@gmail.com>
2020-05-28 00:44:24 +0200

    Skeleton 3

--- app/src/main/java/it/thewalkingthread/dbconstructor/model/Pokemon.java
+++ app/src/main/java/it/thewalkingthread/dbconstructor/model/Pokemon.java
@@ -1,8 +1,36 @@
 package it.thewalkingthread.dbconstructor.model;
 
+import java.util.List;
+
 public class Pokemon {
     //local class to describe pokemon
     int id;
     String name;
-    String type;
+    List<PokeType> types;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<PokeType> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<PokeType> types) {
+        this.types = types;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
