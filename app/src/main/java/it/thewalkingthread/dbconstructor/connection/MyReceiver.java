2f76a224baba75beda8944709714f72de7a3960c
marco <mar.calavaro@gmail.com>
2020-05-30 19:29:52 +0200

    Connection utilities

---
+++ app/src/main/java/it/thewalkingthread/dbconstructor/connection/MyReceiver.java
@@ -0,0 +1,18 @@
+package it.thewalkingthread.dbconstructor.connection;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String status = NetworkUtil.getConnectivityStatusString(context);
+        if(status.isEmpty()) {
+            status="No Internet Connection";
+        }
+        //TODO something for internet connection
+        Toast.makeText(context, status, Toast.LENGTH_LONG).show();
+    }
+}
